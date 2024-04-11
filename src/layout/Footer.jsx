bffd9f81b046d3b1e3f5285f7a9a7c272ac1a7d2
Artem <artiebears@mail.ru>
2024-04-11 18:23:09 +0300

    test

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
 
             <div className="footer-copyright">
                 <div className="container">
-                    © {new Date().getFullYear()} Copyright Text aaaa
+                    © {new Date().getFullYear()} Copyright Text
                     <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
                 </div>
             </div>
